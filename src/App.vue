b945711fa7aba0d11de2e6d1c6b99766c4621f4d
Rinconjr <nicorinconb@yahoo.com>
2024-09-10 19:46:12 -0500

    Agregando front y peticion para pubsub

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <NavBar />
+    <h1>Aplicación Suscriptor</h1>
+    <div>
+      <h2>Mensajes Recibidos:</h2>
+      <ul>
+        <li v-for="(msg, index) in messages" :key="index">{{ msg }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
+  },
+  data() {
+    return {
+      messages: [], // Array para almacenar mensajes recibidos
+      webSocket: null
+    };
+  },
+  mounted() {
+    this.connectWebSocket();
+  },
+  methods: {
+    connectWebSocket() {
+      const wsUrl = 'wss://localhost:7016/ws'; // URL del WebSocket
+      this.webSocket = new WebSocket(wsUrl);
+
+      this.webSocket.onopen = () => {
+        console.log('Conectado al WebSocket');
+      };
+
+      this.webSocket.onmessage = (event) => {
+        // Añadir el mensaje recibido al array de mensajes
+        this.messages.push(event.data);
+      };
+
+      this.webSocket.onclose = () => {
+        console.log('Desconectado del WebSocket');
+      };
+
+      this.webSocket.onerror = (error) => {
+        console.error('Error en WebSocket:', error);
+      };
+    }
   }
 }
 </script>
@@ -17,10 +57,22 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1 {
+  margin-top: 1em;
+  font-size: 2.5em;
+  font-weight: bold;
+  color: #42b983;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 0.5em;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+      <ul>
+        <li><a href="#">Publicador Suscriptor con RabbitMQ, .Net, RabbitMQ y SQL Server </a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </nav>
+</template>
+
+<style>
+nav {
+  background-color: #42b983;
+  padding: 1em;
+}
+
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+nav ul li {
+  display: inline;
+  margin-right: 1em;
+}
+
+nav ul li a {
+  color: white;
+  text-decoration: underline;
+  font-size: 1.2em;
+}
+</style>
